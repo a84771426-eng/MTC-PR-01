a277efc012901c052bf9dd611f581df040195ef9
insane <a84771426@gmail.com>
2026-05-09 02:52:44 -0300

    removendo arquivos

--- script.js
+++
@@ -1,9 +0,0 @@
-const menuToggleButton = document.querySelector("[data-menu-toggle]");
-const mobileMenu = document.querySelector("[data-mobile-menu]");
-
-if (menuToggleButton && mobileMenu) {
-  menuToggleButton.addEventListener("click", () => {
-    const isOpen = mobileMenu.classList.toggle("is-open");
-    menuToggleButton.setAttribute("aria-expanded", String(isOpen));
-  });
-}
