7be8c20008ffdd70c2a23fe293c1787b798ee32b
insane <a84771426@gmail.com>
2026-05-09 21:04:05 -0300

    o que você alterou

---
+++ site-data.js
@@ -0,0 +1,13 @@
+window.SITE = {
+  razaoSocial: "64.727.570 Valquiria Maia Matias",
+  nomeFantasia: "Valquiria Maia Matias",
+  cnpj: "64.727.570/0001-60",
+  endereco:
+    "Avenida Marquês de São Vicente, 405 - Conjunto 1804 - Vila Várzea da Barra Funda - São Paulo/SP - CEP: 01139-001",
+  telefone: "(35) 99714-0666",
+  email: "valquiria.maia.matias@gmail.com",
+  dpo: "valquiria.maia.matias@gmail.com",
+  dpoNome: "Valquiria Maia Matias",
+  dominio: "esclarecimentolegal.blog",
+  ano: 2026,
+};
